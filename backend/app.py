d1c26638a4129fd9780362dca44205457483309a
JeffreyYang6 <yangjeffrey05@gmail.com>
2025-10-04 19:16:48 -0400

    testing api

--- backend/app.py
+++ backend/app.py
@@ -4,14 +4,17 @@ from dotenv import load_dotenv
 import os
 from api.gemini import gemini_bp
 from api.gemini_coin_analysis import coin_analysis_bp
+from api import historical_prices_bp
 
 load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
+# Register blueprints
 app.register_blueprint(gemini_bp, url_prefix='/api/gemini')
 app.register_blueprint(coin_analysis_bp, url_prefix='/api/gemini-coin-analysis')
+app.register_blueprint(historical_prices_bp, url_prefix='/api/historical-prices')
 
 if __name__ == '__main__':
     PORT = int(os.getenv('PORT', 4000))
